d3d37452f95d9887447a2794e22c528f1470a7c2
weitsunglin <wilson.lwt.tw@gmail.com>
2024-02-09 12:19:30 +0800

    framework & Remove Element

---
+++ leetcode/leetcode/removeElement.cpp
@@ -0,0 +1,12 @@
+#include "Solution.h"
+
+int Solution::removeElement(std::vector<int>& nums, int val) {
+    int k = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            ++k;
+        }
+    }
+    return k;
+}
