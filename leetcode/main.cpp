f96659f38bf6509bd8364c304320a0431d7a9825
weitsunglin <wilson.lwt.tw@gmail.com>
2024-02-12 01:30:20 +0800

    註解修正

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -18,12 +18,11 @@ int main() {
     /* 27. Remove Element
         ※ difficulty: easy
         ※ describtion: Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
-        The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
-        Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
-        Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
-        The remaining elements of nums are not important as well as the size of nums.
-        Return k.
-        ※ learning objective: Effectively use existing space to remove elements
+                        The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+                        Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+                        Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+                        The remaining elements of nums are not important as well as the size of nums.
+        ※ learning objective: Effectively use existing space to remove elements.
     */
 
     Solution solution;
@@ -44,7 +43,7 @@ int main() {
     ※ difficulty: easy
     ※ describtion: Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
                     You must implement a solution with a linear runtime complexity and use only constant extra space.
-    ※ learning objective: Using XOR to efficiently find once-only numbers.
+    ※ learning objective: Using XOR to efficiently find once-only numbers, because A⊕0=A & A⊕A=0.
     */
     cout << "Single number"<<endl;
     vector<int> nums6 = { 2, 1, 3, 3, 2 };
