86416a8d45419615925476acb4284dae23ce687d
weitsung <wilson.lwt.tw@gmail.com>
2024-02-18 15:55:29 +0800

    移除註解
    移除註解
    
    Co-Authored-By: p96091123 <159810351+p96091123@users.noreply.github.com>
    Co-Authored-By: dv6230 <67547654+dv6230@users.noreply.github.com>

--- leetcode/RemoveFromList.cpp
+++ leetcode/RemoveFromList.cpp
@@ -3,14 +3,12 @@
 
 ListNode* Solution::removeFromList(ListNode* head, int val) {
 
-    //檢查頭元素是否等於val
     while (head != nullptr && head->val == val) {
         ListNode* temp = head;
         head = head->next;
         delete temp;
     }
 
-    //檢查頭元素後的元素是否等於val
     ListNode* current = head;
     while (current != nullptr && current->next != nullptr) {
         if (current->next->val == val) {
