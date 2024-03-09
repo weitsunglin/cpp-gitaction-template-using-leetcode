60fab50e96989ac7b0cb055443401430f94eed7b
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-03-10 01:09:09 +0800

    Update Solution.h

--- Solution.h
+++ Solution.h
@@ -12,6 +12,7 @@ class Solution {
         ListNode* removeLinkedListElement(ListNode* head, int val);
         int removeDuplicatesSortedArray(vector<int>& nums);
         void reverseString(vector<char>& s);
+        vector<int> TwoSum(vector<int>& nums, int target);
 };
 
 #endif
