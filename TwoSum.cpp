eb01d10f8d9c8571807e44155f4341eceeb8ea4b
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-03-10 01:33:28 +0800

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,7 +1,7 @@
 #include "Solution.h"
-
+#include <unordered_map>
  vector<int> Solution::twoSum(vector<int>& nums, int target) {
-   std::unordered_map<int, int> numMap;
+   unordered_map<int, int> numMap;
    for (int i = 0; i < nums.size(); i++) {
        int complement = target - nums[i];
        if (numMap.find(complement) != numMap.end()) {
