1299b2c9a9f5bfa8bc0375a7f66d1b4e9ccb205a
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-03-17 18:23:41 +0800

    Update and rename Best Time to Buy and Sell Stock.cpp to BestTimetoBuyandSellStock.cpp

---
+++ BestTimetoBuyandSellStock.cpp
@@ -0,0 +1,15 @@
+
+#include "Solution.h"
+#include <vector>
+#include <algorithm> // For std::max
+using namespace std;
+
+int Solution::maxProfit(vector<int>& prices) {
+    int minPrice = INT_MAX;
+    int maxProfit = 0;
+    for (int price : prices) {
+        minPrice = std::min(minPrice, price);
+        maxProfit = std::max(maxProfit, price - minPrice);
+    }
+    return maxProfit;
+}
