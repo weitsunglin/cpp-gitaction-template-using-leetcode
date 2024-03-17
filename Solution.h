55120164f2c44ec2314f493e9c978e436771b234
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-03-17 18:34:47 +0800

    Update Solution.h

--- Solution.h
+++ Solution.h
@@ -15,7 +15,7 @@ class Solution {
         void reverseString(vector<char>& s);
         vector<int> twoSum(vector<int>& nums, int target);
         bool validparentheses(string s);
-        int maxProfit(vector<int>& prices);
+        int BestTimetoBuyandSellStock(vector<int>& prices);
 };
 
 #endif
