c0b49a6a4e0631d93bb9ff86c8f582184e7e3ea0
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-03-22 23:44:28 +0800

    Update LengthOfLastWord.cpp

--- LengthOfLastWord.cpp
+++ LengthOfLastWord.cpp
@@ -1,1 +1,17 @@
+#include "Solution.h"
 
+int Solution::lengthOfLastWord(string s) {
+    int length = 0;
+    int i = s.length() - 1;
+    
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+
+    while (i >= 0 && s[i] != ' ') {
+        length++;
+        i--;
+    }
+
+    return length;
+}
