0f43011ec706cd26b6192827101d8ad39fb4f551
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-04-20 00:55:49 +0800

    Create searchInsert.cpp

---
+++ searchInsert.cpp
@@ -0,0 +1,20 @@
+#include "Solution.h"
+
+int Solution::searchInsert(vector<int>& nums, int target) {
+    int low = 0;
+    int high = nums.size() - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return low;
+}
