b9a21669d444cba1dace49ae9968c59febeebc65
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-04-21 16:24:10 +0800

    Update CommonFunction.h

--- CommonFunction.h
+++ CommonFunction.h
@@ -1,12 +1,12 @@
 #ifndef COMMON_FUNCTION_H
 #define COMMON_FUNCTION_H
-
+using namespace std;
 
 class CommonFunction {
 public:
     void printLinkedList(ListNode* head);
-    void printCharVector(std::vector<char>& v);
-    void printIntVector(std::vector<int>& v);
+    void printCharVector(vector<char>& v);
+    void printIntVector(vector<int>& v);
 };
 
 #endif
