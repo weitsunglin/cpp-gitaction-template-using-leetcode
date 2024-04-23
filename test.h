4431dca992113e60b8966da5452ba72810726884
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-04-23 09:08:14 +0800

    Create test.h

---
+++ test.h
@@ -0,0 +1,15 @@
+// test.h
+#pragma once
+
+void testRemoveElement();
+void testSingleNumber();
+void testRemoveLinkedListElement();
+void testRemoveDuplicatesSortedArray();
+void testReverseString();
+void testTwoSum();
+void testValidParentheses();
+void testBestTimetoBuyandSellStock();
+void testMajorityElement();
+void testLengthOfLastWord();
+void testReverseLinkedList();
+void testSearchInsert();
